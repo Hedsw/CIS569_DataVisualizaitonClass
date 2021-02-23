e53bfdb2ffb4cec75a14473d38e3c81d7e2efe41
rdaniello <70703611+rdaniello@users.noreply.github.com>
2021-02-23 06:51:59 -0500

    Fixed drop placement and list scroll areas

--- d3_function.js
+++ d3_function.js
@@ -1,9 +1,9 @@
 
-let fileNames = getFileNames();
-let dataJSON = [];
-let isDragged = false;
+let fileNames = getFileNames(); // array of file names
+let dataJSON = []; // the files in json object array
+let isDragged = false; // flag to discern between drag and click events
 
-// load the contents of all files
+// load the contents of all files 
 var promises = [];
 fileNames.forEach(function(d){
       promises.push(d3.text(d));
@@ -39,10 +39,12 @@ function main(data){
       drawList();
 }
 
+// list file items in left pane of UI and assign interactive call backs for items
 function drawList(){
+      // select the list svg
       let list = d3.select('#list');
 
-      // add text elements based on current file name list
+      // add text elements using json array data
       list.selectAll('text')
             .data(dataJSON)
             .join('text')
@@ -63,6 +65,7 @@ function drawList(){
                         return false;
                   }
             })
+            // highlight on mouse over
             .on('mouseenter', function (mouseData, d) {
                   try {
                         if(!isDragged){
@@ -80,13 +83,13 @@ function drawList(){
                         }
                   } catch {}
             })
+            // assign drag events to svg item
             .call(d3.drag()
                   .on("start", dragstarted)
                   .on("drag", dragged)
                   .on("end", dragended)
             )
-            // add the filenames to selected files array if clicked
-            // remove if already present
+            // toggle 'selected' field of item when clicked
             .on('click', function(event,d){
                   try{
                         // if filename selected then remove 
@@ -110,21 +113,17 @@ function drawList(){
             });
 }
 
+// fires when list item drag starts
 function dragstarted(d) {
-      console.log("drag started" + event + ":::" + d)
-      d3.select(this)
-            
+      // nothing to do since this may be a click
+      // let click event fire
 }
 
-function dragged(event,d) {
-      console.log("drag");
+// fires when list item is dragged
+function dragged(event) {
 
-      let fff = d3.pointer(event);
-      console.log(fff);
-      //console.log(event.clientX)
-
-      // console.log($("#listDiv").scrollTop());
-      // console.log($("#listDiv").height());
+      // set the flag that element has been dragged - ie not a click
+      isDragged = true;
 
       // get the top and bottom of div scroll position
       let scrollTopY = $("#listDiv").scrollTop();
@@ -132,36 +131,26 @@ function dragged(event,d) {
 
       // get the mouse y position in div - subtract margins and padding
       let currYListPos = scrollTopY  + d3.pointer(event)[1] 
-      console.log(scrollTopY);
-      console.log(scrollBotY);
-      console.log(currYListPos);
 
       // if mouse position is close to top then scroll list div up
-      if(currYListPos < scrollTopY + 100){
-            console.log('setting new scroll')
+      if(currYListPos < scrollTopY + 175 && event.x < 20){
             let newScroll = $("#listDiv").scrollTop() - 20;
             if(newScroll < 0) {newScroll = 0}
             $("#listDiv").scrollTop(newScroll);
       }
 
       // if mouse position is close to bottom then scroll list div down
-      if(currYListPos > scrollBotY - 60){
-            console.log('setting new scroll')
+      if(currYListPos > scrollBotY + 80 && event.x < 20){
             let newScroll = $("#listDiv").scrollTop() + 20;
             if(newScroll > scrollBotY - $("#listDiv").height()) {scrollBotY - $("#listDiv").height()}
             $("#listDiv").scrollTop(newScroll);
       }
 
-      // set the flag that element has been dragged - ie not a click
-      isDragged = true;
-
-      // selected the hover text by class - remove old detail hover
+      // selected the hover text by class - remove old detail hover from right pane
       let detail = d3.select('#detail');
       detail.selectAll('.detailHover').remove();
 
-      
-
-      // if in list box
+      // if in list box - left pane
       if(event.x < 20){
             // adjust x and y of object being dragged
             d3.select(this)
@@ -178,7 +167,7 @@ function dragged(event,d) {
             let list = d3.select("#list")
 
             // remove old arrows
-            list.selectAll('.listLine').remove();
+            list.selectAll('.listArrow').remove();
             // create d3 path generator
             let path = d3.path();
             path.moveTo(0,dropY - .5);
@@ -188,12 +177,11 @@ function dragged(event,d) {
             // append path (arrow) to list
             list.append('path')
                   .attr('d',path)
-                  .classed('listLine',true);
+                  .classed('listArrow',true);
             }
-      // then add '+' to detail pane
+      // if in right pane - then add '+' to detail pane
       else{
             let cood = d3.pointer(event)
-            console.log(cood);
 
             // get the number of selected files
             let selectedCount = getSelected().length;
@@ -222,20 +210,14 @@ function dragended(event, d) {
       if(isDragged){
             // remove old arrows
             let list = d3.select("#list")
-            list.selectAll('.listLine').remove();
+            list.selectAll('.listArrow').remove();
 
             // get the index of selected file
             let idx = dataJSON.indexOf(d);
 
-            let selectedFile  = d;
-            console.log("drag ended");
-
-            let newY = Math.round(event.y);
-
             // decide if we are re-ordering list or displaying file
             // based on how far right the object is dragged
             if(event.x < 20){ 
-                  //
                   // put the element back where is belongs
                   d3.select(this)
                         .attr('x', 1)
@@ -244,15 +226,20 @@ function dragended(event, d) {
 
                   // rearrange the filesName array based on new order
                   // get the new index from the event's 'y'
-                  let newY = Math.ceil(event.y);
+                  let newY = Math.round(event.y);
                   if(newY < 0){newy = 0};
                   if(newY > dataJSON.length -1 ){newY = dataJSON.length -1};
+
+                  // if drop is below index of dragged item idx is off by one - fix it
+                  if (newY > idx){
+                        newY --;
+                  }
                   
                   // remove i from fileNames array
                   dataJSON.splice(idx,1);
 
                   // splice filenames array in correct order
-                  dataJSON.splice(newY,0,selectedFile);
+                  dataJSON.splice(newY, 0, d);
 
                   // if dragged item is selected then reflect this change
                   // in detail pane
@@ -276,27 +263,25 @@ function dragended(event, d) {
 
 // draw the detail pane with selected files
 function displaySelectedFiles(){
-      // reordering the list in left pane will order the 
+      // reordering the list in left pane will reorder the 
       // detail pane as well
-
       let detail = d3.select('#detail');
             
+      // get files where 'selected' is true
       let selectedFiles = getSelected();
 
       // empty the pane
       detail.selectAll('div').remove();
 
-      // add the text for each document
+      // add div for each selected file in list to right pane
       var detailDivs = detail.selectAll('div')
             .data(selectedFiles)
             .enter()
             .append("div")
             .style("font", "14px 'Helvetica Neue'")
             .classed('detailDiv',true)
-            // change the file 'selected value'
-            // will remove from right and deselect in list
       
-            
+      // add 'x' to close and deselect item
       detailDivs
             .append('div')
             .text(function(d){return 'X'})
@@ -305,7 +290,6 @@ function displaySelectedFiles(){
             .on('click', function(event,d){
                   // find the item in the data array
                   let item = dataJSON[dataJSON.indexOf(d)];
-                  console.log(item);
                   d.selected = 0;
 
                   // update the list and detail panes
@@ -313,7 +297,7 @@ function displaySelectedFiles(){
                   displaySelectedFiles();
             })
 
-      // add title andcontents to each div
+      // add title and contents to each div
       detailDivs
             .append('div')
             .text(function(d){return "File: " + d.name.split("/")[1]})
@@ -326,6 +310,7 @@ function displaySelectedFiles(){
             .style('padding-top', '10px')
 }
 
+// return data json item where selected equals 1
 function getSelected(){
       let sel = dataJSON.filter(function(item){
             return item.selected === 1;
@@ -333,21 +318,12 @@ function getSelected(){
       return sel;
 }
 
-function getFileContents(fName){
-      let r=null;
-      d3.text('dataset/CIA_19',function(error,data)
-      {
-            if(error){
-                  return console.error(error);
-            }
-            r= data;
-      });
-      return r;
-}
-
+// builds and returns array of json objects to be consumed by D3
 function buildJSON(fileContents){
       var filenamesJSON = [];
 
+      // for each item in fileNames array,
+      // build and add a json object to filenamesJSON
       fileNames.map(function(item, index){
             let fileTxt = fileContents[index];
 
@@ -361,6 +337,7 @@ function buildJSON(fileContents){
       return filenamesJSON;
 }
 
+// returns the filenames for project
 function getFileNames(){
       var filenames = [
             'dataset/CIA_01','dataset/CIA_02','dataset/CIA_03','dataset/CIA_04','dataset/CIA_05','dataset/CIA_06',
